2b7491ba14a8aef2cb78f69fca407c4a38377778
victorac <victorde.ac@gmail.com>
2021-11-15 03:32:49 +0000

    save results to output.json

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # raspadinha-da-web
 
-`node extract_tables.js`
+Crawl and extract tables from [estatísticas SSP](http://www.ssp.sp.gov.br/Estatistica/Pesquisa.aspx)
+
+Run command: `node select.js`
+
+Results are saved to **output.json**
+
--- select.js
+++ select.js
@@ -4,52 +4,6 @@ import fs from "fs"
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-async function get_level_info(selector, page) {
-    await page.waitForSelector(selector)
-    let content_type = await page.$(selector);
-    const level_name = await content_type.evaluate(node => node.textContent);
-    const parent = await content_type.$x("..")
-    const level_items = await parent[0].$$(".list-group-item")
-    const n_items = level_items.length
-    return [level_name, n_items];
-}
-
-async function get_item(index, selector, page) {
-    await page.screenshot( {"path":`get_item_${index}.png`})
-    await page.waitForSelector(selector)
-    let content_type = await page.$(selector);
-    const parent = await content_type.$x("..")
-    const level_items = await parent[0].$$(".list-group-item")
-    return level_items[index];
-}
-
-async function crawl(selector, page, level) {
-    if (level > max_level) {
-        console.log (`max level = ${level}; goBack`);
-        return {};
-    } 
-    const data = {}
-    await page.screenshot({"path": "crawl.png"})
-    const tables = await extract_tables(page)
-    data["tables"] = tables;
-    const [level_name, n_items] = await get_level_info(selector, page)
-    console.log(`level name: ${level_name}`);
-    for (let index = 1; index < n_items; index++) {
-        console.log(`index: ${index}; level name: ${level_name}; n_items: ${n_items}`)
-        const item = await get_item(index, selector, page)
-        const item_name = await item.evaluate(node => node.textContent);
-        console.log(`item name: ${item_name}`);
-        await item.click();
-        await crawl(selector, page, level + 1);
-        console.log(`back from crawl - current level ${level}; current index ${index}`);
-        await page.goBack();
-        await page.screenshot({"path": "goBack.png"})
-        await delay(5000);
-    }
-    return data;
-}
-
-const max_level = 1;
 const link = 'http://www.ssp.sp.gov.br/Estatistica/Pesquisa.aspx';
 (async () => {
     const browser = await puppeteer.launch({
